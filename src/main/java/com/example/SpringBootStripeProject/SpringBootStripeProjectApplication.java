7aa3be13d6eb540d8b76f70ebccfb0f9a884b83b
Aditya <aditya.app.1993@gmail.com>
2020-11-03 14:24:20 +0400

    first commit

---
+++ src/main/java/com/example/SpringBootStripeProject/SpringBootStripeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootStripeProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStripeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootStripeProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringBootStripeProject/SpringBootStripeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootStripeProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStripeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
